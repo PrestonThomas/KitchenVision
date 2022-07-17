acab7a351bb431ed805f9d7148fc39d75050e413
Yit Soo <yjsoo004@mymail.sim.edu.sg>
2022-07-17 22:43:25 +0800

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/screens/InventoryScreen.js 	modified:   yarn.lock

--- src/screens/InventoryScreen.js
+++ src/screens/InventoryScreen.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Button, StyleSheet,TouchableOpacity, ActivityIndicator, Pressable } from 'react-native';
+import { Text, View, Button, StyleSheet,TouchableOpacity, ActivityIndicator, Pressable,SafeAreaView,Switch, ScrollView } from 'react-native';
+//import for the animation of Collapse and Expand
+import * as Animatable from 'react-native-animatable';
+//import for the collapsible/Expandable view
+import Collapsible from 'react-native-collapsible';
+//import for the Accordion view
+import Accordion from 'react-native-collapsible/Accordion';
 import scanner from '../components/Scanner';
 import FAB from 'react-native-fab';
 import Barcode from '../api/barcode';
@@ -8,6 +14,35 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import NameForm from '../components/ItemDetail';
 // Still working on getting the text to update/return upon camera close. Looking at async functions and promises. - Preston;
+
+
+//Dummy content to show
+//You can also use dynamic data by calling web service
+const CONTENT = [
+    {
+        title: 'Dairy',
+        content:
+        'The following terms and conditions, together with any referenced documents (collectively, "Terms of Use") form a legal agreement between you and your employer, employees, agents, contractors and any other entity on whose behalf you accept these terms (collectively, “you” and “your”), and ServiceNow, Inc. (“ServiceNow,” “we,” “us” and “our”).',
+    },
+    {
+        title: 'Privacy Policy',
+        content:
+        'A Privacy Policy agreement is the agreement where you specify if you collect personal data from your users, what kind of personal data you collect and what you do with that data.',
+    },
+    {
+        title: 'Return Policy',
+        content:
+        'Our Return & Refund Policy template lets you get started with a Return and Refund Policy agreement. This template is free to download and use.According to TrueShip study, over 60% of customers review a Return/Refund Policy before they make a purchasing decision.',
+    },
+];
+
+//To make the selector (Something like tabs)
+// const SELECTORS = [
+//     { title: 'T&C', value: 0 },
+//     { title: 'Privacy Policy', value: 1 },
+//     { title: 'Return Policy', value: 2 },
+//     { title: 'Reset all' },
+// ];
 
 let barcodeOutput;
 
@@ -34,57 +69,171 @@ class InventoryScreen extends React.Component {
 }
 
 function InventoryHome({ navigation }) {
+
+    // Ddefault active selector
+    const [activeSections, setActiveSections] = useState([]);
+    // Collapsed condition for the single collapsible
+    const [collapsed, setCollapsed] = useState(true);
+    // MultipleSelect is for the Multiple Expand allowed
+    // True: Expand multiple at a time
+    // False: One can be expand at a time
+    const [multipleSelect, setMultipleSelect] = useState(false);
+
+    const toggleExpanded = () => {
+        //Toggling the state of single Collapsible
+        setCollapsed(!collapsed);
+    };
+
+    const setSections = (sections) => {
+        //setting up a active section state
+        setActiveSections(sections.includes(undefined) ? [] : sections);
+    };
+
+    const renderHeader = (section, _, isActive) => {
+    //Accordion Header view
     return (
-        <>
-        <View style={styles.container}>
-            <Text style={styles.name}>Product Category</Text>
-            <View style={styles.rect}>
-                <Text style={styles.itemsName}>Item&#39;s Name</Text>
-                <Text style={styles.date}>Date</Text>
-                <View style={styles.inputBox}>
-                    <TouchableOpacity style={styles.plusButton}>
-                        <Button title='+'/>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.minusButton}>
-                        {/* <Text style={styles.minusText}>−</Text> */}
-                        <Button title='−'/>
-                    </TouchableOpacity>
-                </View>
-                {/* <View style={styles.rect2}>
+        <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor">
+        <Text style={styles.headerText}>{section.title}</Text>
+        </Animatable.View>
+    );
+    };
+
+    const renderContent = (section, _, isActive) => {
+    //Accordion Content view
+    return (
+        <Animatable.View
+        duration={400}
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor">
+        <Animatable.Text
+            animation={isActive ? 'bounceIn' : undefined}
+            style={{ textAlign: 'center' }}>
+            {section.content}
+        </Animatable.Text>
+        </Animatable.View>
+    );
+    };
+
+    return (
+        <><>
+            <View style={styles.container}>
+                <Text style={styles.name}>Product Category</Text>
+                <View style={styles.rect}>
                     <Text style={styles.itemsName}>Item&#39;s Name</Text>
+                    <Text style={styles.date}>Date</Text>
+                    <View style={styles.inputBox}>
+                        <TouchableOpacity style={styles.plusButton}>
+                            <Button title='+' />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.minusButton}>
+                            {/* <Text style={styles.minusText}>−</Text> */}
+                            <Button title='−' />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={styles.rect3}>
-                    <View style={styles.rect4}>
-                        <Text style={styles.date}>Date</Text>
-                    </View>
-                    <View style={styles.rect5}>
-                    <TouchableOpacity style={styles.plusButton}>
-                        <Text style={styles.plusText}>+</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.minusButton}>
-                        <Text style={styles.minusText}>−</Text>
-                    </TouchableOpacity>
-                    </View>
-                </View> */}
             </View>
-        </View>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30, color: 'black' }}>
-            InventoryScreen
-        </Text>
-        <Button
-            onPress={() => {
-                console.log(scanner.returnScannedText());
-            } }
-            title="Retrieve Expiry Date" />
-        <Button
-            onPress={() => navigation.navigate('Barcode Scanner')}
-            title="Scan Barcode" />
-        <Button
-            // style align to the bottom of the screen
-            onPress={() => console.log(barcodeOutput[0].barcodeText)}
-            title="Log barcode output" />
-    </View></>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ fontSize: 30, color: 'black' }}>
+                    InventoryScreen
+                </Text>
+                <Button
+                    onPress={() => {
+                        console.log(scanner.returnScannedText());
+                    } }
+                    title="Retrieve Expiry Date" />
+                <Button
+                    onPress={() => navigation.navigate('Barcode Scanner')}
+                    title="Scan Barcode" />
+                <Button
+                    // style align to the bottom of the screen
+                    onPress={() => console.log(barcodeOutput[0].barcodeText)}
+                    title="Log barcode output" />
+            </View></>
+            
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={styles.containerA}>
+                    <ScrollView>
+                        {/* <Text style={styles.title}>
+                            Example of Collapsible/Accordion/Expandable List View in React
+                            Native
+                        </Text> */}
+
+                        {/*Code for Single Collapsible Start*/}
+                        {/* <TouchableOpacity onPress={toggleExpanded}>
+                            <View style={styles.header}>
+                                <Text style={styles.headerText}>Single Collapsible</Text>
+                                Heading of Single Collapsible
+                            </View>
+                        </TouchableOpacity> */}
+                        {/*Content of Single Collapsible*/}
+                        {/* <Collapsible collapsed={collapsed} align="center">
+                            <View style={styles.content}>
+                                <Text style={{ textAlign: 'center' }}>
+                                    This is a dummy text of Single Collapsible View
+                                </Text>
+                            </View>
+                        </Collapsible> */}
+                        {/*Code for Single Collapsible Ends*/}
+
+                        {/* <View style={{ backgroundColor: '#000', height: 1, marginTop: 10 }} /> */}
+                        <View style={styles.multipleToggle}>
+                            <Text style={styles.multipleToggle__title}>
+                                Multiple Expand Allowed?
+                            </Text>
+                            <Switch
+                                value={multipleSelect}
+                                onValueChange={(multipleSelect) => setMultipleSelect(multipleSelect)} />
+                        </View>
+                        <Text style={styles.selectTitle}>
+                            Please select below option to expand
+                        </Text>
+
+                        {/*Code for Selector starts here*/}
+                        {/* <View style={styles.selectors}>
+                            {SELECTORS.map((selector) => (
+                                <TouchableOpacity
+                                    key={selector.title}
+                                    onPress={() => setSections([selector.value])}
+                                >
+                                    <View style={styles.selector}>
+                                        <Text
+                                            style={activeSections.includes(selector.value) &&
+                                                styles.activeSelector}>
+                                            {selector.title}
+                                        </Text>
+                                    </View>
+                                </TouchableOpacity>
+                            ))}
+                        </View> */}
+                        {/*Code for Selector ends here*/}
+
+                        {/*Code for Accordion/Expandable List starts here*/}
+                        <Accordion
+                            activeSections={activeSections}
+                            //for any default active section
+                            sections={CONTENT}
+                            //title and content of accordion
+                            touchableComponent={TouchableOpacity}
+                            //which type of touchable component you want
+                            //It can be the following Touchables
+                            //TouchableHighlight, TouchableNativeFeedback
+                            //TouchableOpacity , TouchableWithoutFeedback
+                            expandMultiple={multipleSelect}
+                            //Do you want to expand mutiple at a time or single at a time
+                            renderHeader={renderHeader}
+                            //Header Component(View) to render
+                            renderContent={renderContent}
+                            //Content Component(View) to render
+                            duration={400}
+                            //Duration for Collapse and expand
+                            onChange={setSections} />
+                        {/*Code for Accordion/Expandable List ends here*/}
+                    </ScrollView>
+                </View>
+            </SafeAreaView></>
 
     );
 }
@@ -177,35 +326,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#000000',
         flexDirection: 'row',
-        // justifyContent:'space-between',
     },
-    // rect2: {
-    //     backgroundColor: 'rgba(255,255,255,1)',
-    //     borderWidth: 1,
-    //     borderColor: '#000000',
-    //     padding: 0,
-    //     margin: 0,
-    //     width: '40%',
-    // },
-    // rect3: {
-    //     flex: 0.5,
-    //     backgroundColor: 'rgba(248, 248, 248,1)',
-    //     flexDirection: 'row',
-    // },
-    // rect4: {
-    //     backgroundColor: 'rgba(255,255,255,1)',
-    //     borderWidth: 1,
-    //     borderColor: '#000000',
-    //     marginLeft: 0,
-    //     width: '40%',
-    // },
-    // rect5: {
-    //     backgroundColor: 'rgba(255,255,255,1)',
-    //     borderWidth: 1,
-    //     borderColor: '#000000',
-    //     marginLeft: 0,
-    //     width: '60%',
-    // },
     date: {
         top: 15,
         left: 15,
@@ -231,14 +352,6 @@ const styles = StyleSheet.create({
         position: 'absolute',
         backgroundColor: 'rgba(88,138,240,1)',
     },
-    // plusText: {
-    //     fontFamily: 'roboto-regular',
-    //     color: 'rgba(255,255,255,1)',
-    //     fontSize: 25,
-    //     marginTop: 0,
-    //     marginLeft: 8,
-    //     position: 'absolute',
-    // },
     minusButton: {
         margin:15,
         width: '25%',
@@ -246,13 +359,6 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(88,138,240,1)',
         alignSelf:'flex-end',
     },
-    // minusText: {
-    //     fontFamily: 'roboto-regular',
-    //     color: 'rgba(255,255,255,1)',
-    //     fontSize: 25,
-    //     marginLeft: 8,
-    //     position: 'absolute',
-    // },
     bcScanButton: {
         alignItems: 'center',
         justifyContent: 'center',
@@ -268,6 +374,66 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         letterSpacing: 0.25,
         color: 'white',
+      },
+
+
+      containerA: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        paddingTop: 30,
+      },
+    //   title: {
+    //     textAlign: 'center',
+    //     fontSize: 18,
+    //     fontWeight: '300',
+    //     marginBottom: 20,
+    //   },
+      header: {
+        backgroundColor: '#F5FCFF',
+        padding: 10,
+      },
+      headerText: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: '500',
+      },
+      content: {
+        padding: 20,
+        backgroundColor: '#fff',
+      },
+      active: {
+        backgroundColor: 'rgba(255,255,255,1)',
+      },
+      inactive: {
+        backgroundColor: 'rgba(245,252,255,1)',
+      },
+    //   selectors: {
+    //     marginBottom: 10,
+    //     flexDirection: 'row',
+    //     justifyContent: 'center',
+    //   },
+    //   selector: {
+    //     backgroundColor: '#F5FCFF',
+    //     padding: 10,
+    //   },
+    //   activeSelector: {
+    //     fontWeight: 'bold',
+    //   },
+      selectTitle: {
+        fontSize: 14,
+        fontWeight: '500',
+        padding: 10,
+        textAlign: 'center',
+      },
+      multipleToggle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: 30,
+        alignItems: 'center',
+      },
+      multipleToggle__title: {
+        fontSize: 16,
+        marginRight: 8,
       },
   });
 export default InventoryScreen;
