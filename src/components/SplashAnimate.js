75ccad954c4c84e8485a180f2e024c0e7e8378dc
Preston Thomas <thomas007@mymail.sim.edu.sg>
2022-08-09 17:32:50 +0800

    Updated splashscreen with iOS compatibility

--- src/components/SplashAnimate.js
+++ src/components/SplashAnimate.js
@@ -33,7 +33,7 @@ export default class Splash extends Component {
                     flex: 0.5,
                     backgroundColor: '#FAC898'
                 }}>
-                <Animatable.Text animation="pulse" easing="ease-in" duration={2000} iterationCount="infinite" direction="alternate" style={{ textAlign: 'center', fontSize:45, fontFamily:"Veggy", color:"black", marginBottom:30 }}>KITCHENVISION</Animatable.Text>
+                <Animatable.Text animation="pulse" easing="ease-in" duration={2000} iterationCount="infinite" direction="alternate" style={{ textAlign: 'center', fontSize:45, fontFamily:"VEGgYPERSONALUSE-SemiexpandedMe", color:"black", marginBottom:30 }}>KITCHENVISION</Animatable.Text>
                 <Animatable.Text animation="jello" easing="ease-in" duration={2000} iterationCount="infinite" style={{ textAlign: 'center', fontSize:32 }}>🍞🍞🍞</Animatable.Text>
             </View>
             {/* <View style={{
